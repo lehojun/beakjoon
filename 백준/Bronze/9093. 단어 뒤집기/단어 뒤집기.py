072545e5f526b242f5b739cbc794ba8b1b664dab
lehojun <151193867+lehojun@users.noreply.github.com>
2025-03-09 01:58:13 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 148 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -7,7 +7,6 @@ command = [read().strip() for _ in range(N)]
 for i in range(len(command)):
   command_list = command[i].split()
   result = ''
-  reverse_words = []
 
   for j in range(len(command_list)):
     reverse_word = ''.join(reversed(command_list[j]))
